97f11084ae61b0f4e9b2880faec3f04f3753843f
Connor Michael Gerrits <connormgerrits@connors-air.worcester.local>
2019-10-11 18:41:08 -0400

    Fixed four functions as instructed

--- hazyCalculator.js
+++ hazyCalculator.js
@@ -1,18 +1,21 @@
 function isSkippedValue(value) {
-  return !value
+  if (value == '' || value === undefined) {
+    return true
+  }
+  return false
 }
 
 function isNumericValue(value) {
-  return !isNaN(value)
+  return !isNaN(value) && value !== ''
 }
 
 function isNothingValue(value) {
-  return value === null
+  return value == null
 }
 
 function isAcceptableValue(value) {
   const operators = ['+', '-', '*', '/']
-  return typeof value === Number || operators.includes(value)
+  return typeof Number(value) === 'number' || operators.includes(value)
 }
 
 function performCalculationStep(firstOperand, operator, secondOperand) {
@@ -58,3 +61,21 @@ function calculate(calculationSteps) {
 }
 
 module.exports = calculate
+
+/*
+## Instructions
+
+Currently supporting functions (`isSkippedValue`, `isNumericValue`, `isNothingValue`, and `isAcceptableValue`) used by our calculator are causing the tests to fail. 
+Your task is to rework these 4 functions so that they work as expected, making the tests pass.
+
+## Calculation Rules
+The `calculate` function runs an arithmetic calculation based upon an array of inputs (eg. `[2, '*', 10]`)
+
+Each input should be either a number or an operand (`+`, `-`, `*`, `/`)
+
+Our upstream data is inconsistent, so we _intentionally_ accept it and deal with it in the following ways:
+* Stringified numbers (eg. `'2'`) should be treated as numbers
+* `NULL` should be treated as zero
+* `undefined` values should be ignored
+* Empty string (eg. `''`) values should be ignored
+*/
